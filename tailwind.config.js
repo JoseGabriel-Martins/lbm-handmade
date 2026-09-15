b5b3b42044b9733dd71f6f21272b47f7f6a466c7
Jose Gabriel <josegabriel.contato01@gmail.com>
2026-09-15 08:55:57 -0300

    update: modulariza estilos em styles.css e tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        espresso: '#261810',
+        caramelo: '#C48B5E',
+        linho: '#FAF8F5',
+        areia: '#EAE4DC',
+      },
+      fontFamily: {
+        script: ['"Pacifico"', 'cursive'],
+        sans: ['"Plus Jakarta Sans"', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+};
